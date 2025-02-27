de4a033b30237b37250c8b48400d4244fb4f146f
Jaya rudhra murthy <jayarudhramurthynunna@gmail.com>
2025-02-28 01:15:41 +0530

    Add files via upload

---
+++ rudhra1.java
@@ -0,0 +1,9 @@
+Class rudhra
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Name:N Jaya Rudhra Murthy ");
+			System.out.println("CLASS: CSE");
+			System.out.println("SECTION:C");
+		}
+}
